3bf40fe3e3ee9f7321dd6da75a457e7f5b5a8ca4
Luca Bello <luca.bello@canonical.com>
2023-08-30 11:56:02 +0200

    update tests

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -22,78 +22,9 @@ async def get_unit_address(ops_test: OpsTest, app_name: str, unit_num: int) -> s
     return status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["address"]
 
 
-def interleave(l1: list, l2: list) -> list:
-    """Interleave two lists.
-
-    >>> interleave([1,2,3], ['a', 'b', 'c'])
-    [1, 'a', 2, 'b', 3, 'c']
-
-    Reference: https://stackoverflow.com/a/11125298/3516684
-    """
-    return [x for t in zip(l1, l2) for x in t]
-
-
-async def cli_upgrade_from_path_and_wait(
-    ops_test: OpsTest,
-    path: str,
-    alias: str,
-    resources: Dict[str, str] = None,
-    wait_for_status: str = None,
-):
-    if resources is None:
-        resources = {}
-
-    resource_pairs = [f"{k}={v}" for k, v in resources.items()]
-    resource_arg_prefixes = ["--resource"] * len(resource_pairs)
-    resource_args = interleave(resource_arg_prefixes, resource_pairs)
-
-    cmd = [
-        "juju",
-        "refresh",
-        "--path",
-        path,
-        alias,
-        *resource_args,
-    ]
-
-    retcode, stdout, stderr = await ops_test.run(*cmd)
-    assert retcode == 0, f"Upgrade failed: {(stderr or stdout).strip()}"
-    logger.info(stdout)
-    await ops_test.model.wait_for_idle(apps=[alias], status=wait_for_status, timeout=120)
-
-
-async def get_leader_unit_num(ops_test: OpsTest, app_name: str):
-    units = ops_test.model.applications[app_name].units
-    is_leader = [await units[i].is_leader_from_status() for i in range(len(units))]
-    logger.info("Leaders: %s", is_leader)
-    return is_leader.index(True)
-
-
-async def is_leader_elected(ops_test: OpsTest, app_name: str):
-    units = ops_test.model.applications[app_name].units
-    return any([await units[i].is_leader_from_status() for i in range(len(units))])
-
-
-async def block_until_leader_elected(ops_test: OpsTest, app_name: str):
-    # await ops_test.model.block_until(is_leader_elected)
-    # block_until does not take async (yet?) https://github.com/juju/python-libjuju/issues/609
-    while not await is_leader_elected(ops_test, app_name):
-        await asyncio.sleep(5)
-
-
-def uk8s_group() -> str:
-    try:
-        # Classically confined microk8s
-        uk8s_group = grp.getgrnam("microk8s").gr_name
-    except KeyError:
-        # Strictly confined microk8s
-        uk8s_group = "snap_microk8s"
-    return uk8s_group
-
-
 async def is_blackbox_unit_up(ops_test: OpsTest, app_name: str, unit_num: int):
     address = await get_unit_address(ops_test, app_name, unit_num)
-    url = f"http://{address}:9093"
+    url = f"http://{address}:9115"
     logger.info("blackbox exporter public address: %s", url)
 
     response = urllib.request.urlopen(f"{url}", data=None, timeout=2.0)
--- tests/integration/test_scale_charm.py
+++ tests/integration/test_scale_charm.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 import pytest
 import yaml
-from helpers import block_until_leader_elected, get_leader_unit_num, is_blackbox_up
+from helpers import is_blackbox_up
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
@@ -37,26 +37,4 @@ async def test_build_and_deploy(ops_test: OpsTest, charm_under_test):
     )
     await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
     assert ops_test.model.applications[app_name].units[0].workload_status == "active"
-    assert await is_blackbox_up(ops_test, app_name)
-
-
-# @pytest.mark.abort_on_fail
-@pytest.mark.xfail
-async def test_scale_up_from_single_unit(ops_test: OpsTest):
-    """Add a few more units."""
-    await ops_test.model.applications[app_name].scale(scale_change=2)
-    await ops_test.model.wait_for_idle(
-        apps=[app_name], status="active", timeout=1000, wait_for_exact_units=3
-    )
-    assert await is_blackbox_up(ops_test, app_name)
-
-
-# @pytest.mark.abort_on_fail
-@pytest.mark.xfail
-async def test_scale_down_to_single_unit_without_leadership_change(ops_test):
-    """Remove a few units."""
-    await ops_test.model.applications[app_name].scale(scale_change=-2)
-    await ops_test.model.wait_for_idle(
-        apps=[app_name], status="active", timeout=1000, wait_for_exact_units=1
-    )
     assert await is_blackbox_up(ops_test, app_name)
